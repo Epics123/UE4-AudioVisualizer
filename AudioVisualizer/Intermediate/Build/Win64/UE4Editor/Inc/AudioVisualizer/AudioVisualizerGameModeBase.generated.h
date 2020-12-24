93d5d60a97e13c219d50a951419b40a16e74876e
Ethan Heil <eheil20@gmail.com>
2020-12-24 08:28:08 -0500

    Adding base UE4 project

---
+++ AudioVisualizer/Intermediate/Build/Win64/UE4Editor/Inc/AudioVisualizer/AudioVisualizerGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef AUDIOVISUALIZER_AudioVisualizerGameModeBase_generated_h
+#error "AudioVisualizerGameModeBase.generated.h already included, missing '#pragma once' in AudioVisualizerGameModeBase.h"
+#endif
+#define AUDIOVISUALIZER_AudioVisualizerGameModeBase_generated_h
+
+#define AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_SPARSE_DATA
+#define AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_RPC_WRAPPERS
+#define AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAAudioVisualizerGameModeBase(); \
+	friend struct Z_Construct_UClass_AAudioVisualizerGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AAudioVisualizerGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/AudioVisualizer"), NO_API) \
+	DECLARE_SERIALIZER(AAudioVisualizerGameModeBase)
+
+
+#define AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAAudioVisualizerGameModeBase(); \
+	friend struct Z_Construct_UClass_AAudioVisualizerGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AAudioVisualizerGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/AudioVisualizer"), NO_API) \
+	DECLARE_SERIALIZER(AAudioVisualizerGameModeBase)
+
+
+#define AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AAudioVisualizerGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AAudioVisualizerGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAudioVisualizerGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAudioVisualizerGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAudioVisualizerGameModeBase(AAudioVisualizerGameModeBase&&); \
+	NO_API AAudioVisualizerGameModeBase(const AAudioVisualizerGameModeBase&); \
+public:
+
+
+#define AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AAudioVisualizerGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AAudioVisualizerGameModeBase(AAudioVisualizerGameModeBase&&); \
+	NO_API AAudioVisualizerGameModeBase(const AAudioVisualizerGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AAudioVisualizerGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AAudioVisualizerGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AAudioVisualizerGameModeBase)
+
+
+#define AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_12_PROLOG
+#define AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_SPARSE_DATA \
+	AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_RPC_WRAPPERS \
+	AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_INCLASS \
+	AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_SPARSE_DATA \
+	AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> AUDIOVISUALIZER_API UClass* StaticClass<class AAudioVisualizerGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID AudioVisualizer_Source_AudioVisualizer_AudioVisualizerGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ AudioVisualizer/Source/AudioVisualizer/AudioVisualizerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AudioVisualizerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AUDIOVISUALIZER_API AAudioVisualizerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
